d27d05b05c6854acdaa5b7bd4090835dc2e45ee9
nikshaan <137101877+Nikshaan@users.noreply.github.com>
2024-12-07 20:13:07 +0530

    add: gallery

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,29 +9,29 @@ const Footer = () => {
   return (
     <div className="bg-black pt-10 font-raleway text-white flex flex-col justify-center items-center">
         <div className="flex flex-col justify-center items-center p-3">
-            <p className="text-center font-semibold px-3">SUBSCRIBE TO RECEIVE POSTS DIRECTLY TO YOUR INBOX</p>
-            <div className="flex w-full px-4 gap-4 justify-center items-center mt-4">
+            <p className="text-center font-semibold px-3 sm:text-2xl">SUBSCRIBE TO RECEIVE POSTS DIRECTLY TO YOUR INBOX</p>
+            <div className="flex w-full sm:text-xl px-4 gap-4 justify-center items-center mt-4">
                 <input type="text" placeholder="Enter your email address" className="px-2 py-1 w-full rounded-sm text-black"/>
                 <button className="border-2 px-2 py-0.5 text-nowrap rounded-sm hover:bg-white hover:text-black hover:border-black duration-300 transition-all"> Get notified!</button>
             </div>
         </div>
-        <div className="flex gap-4 my-10">
+        <div className="flex gap-4 sm:gap-6 my-10">
             <Link to="https://www.facebook.com/sharmila.sharma.7739">
-              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Facebook" src={facebook} alt="facebook" className="w-8 h-8 lg:w-10 lg:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
+              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Facebook" src={facebook} alt="facebook" className="w-8 h-8 sm:w-10 sm:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
             </Link>
             <Link to="https://x.com/sharmilaart">
-              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Twitter" src={twitter} alt="twitter" className="w-8 h-8 lg:w-10 lg:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
+              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Twitter" src={twitter} alt="twitter" className="w-8 h-8 sm:w-10 sm:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
             </Link>
             <Link to="https://www.instagram.com/sharmila.sharma31/">
-              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Instagram" src={instagram} alt="instagram" className="w-8 h-8 lg:w-10 lg:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
+              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Instagram" src={instagram} alt="instagram" className="w-8 h-8 sm:w-10 sm:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
             </Link>
             <Link to="mailto:sharmila.sharma31@gmail.com">
-              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Email" src={gmail} alt="gmail" className="w-8 h-8 lg:w-10 lg:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
+              <img data-tooltip-id="my-tooltip" data-tooltip-place="bottom" data-tooltip-content="Email" src={gmail} alt="gmail" className="w-8 h-8 sm:w-10 sm:h-10 cursor-pointer hover:scale-110 duration-200 hover:shadow-md hover:shadow-white rounded-full border-2 border-white"/>
             </Link>
             <Tooltip id="my-tooltip" />
           </div>
         <div className="mb-10">
-            <p className="font-thin">Copyright © Sharmila Sharma 2024</p>
+            <p className="font-thin sm:text-xl">Copyright © Sharmila Sharma 2024</p>
         </div>
     </div>
   )
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,66 @@
-
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import cypress from "../assets/cypress.jpg";
+import gal1 from "../assets/gal1.jpg";
+import gal2 from "../assets/gal2.jpeg";
+import gal3 from "../assets/gal3.jpeg";
+import { Link } from "react-router-dom";
+import { motion } from "motion/react";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <>
+    <div className="absolute top-0 right-0 left-0">
+        <Navbar />
+    </div>
+    <div className="bg-[#5F823C] h-full flex flex-col justify-center sm:pb-16">
+    <img src={cypress} className="mt-16"/>
+      <div className="bg-black relative border-2 border-white bg-opacity-65 p-4 rounded-xl mx-4 md:mx-10 lg:mx-16 -mt-40 sm:-mt-[30rem] lg:-mt-[40rem] xl:-mt-[53rem] mb-14 h-3/4 flex justify-center items-center flex-col">
+        <h1 className="text-white font-thin text-9xl font-madi -mt-20 gallery absolute -top-1">Gallery</h1>
+        <div className="overflow-hidden font-semibold font-raleway mt-20 mb-5 flex flex-col sm:flex-row gap-10 sm:items-start">
+          <motion.div
+          initial = {{ opacity: 0}}
+          animate = {{opacity: 1}}
+          transition={{duration: 2}}
+          className="relative sm:w-[170%]">
+            <img src={gal1} className="rounded-md border-2 shadow-md shadow-green-300"/>
+            <Link to="/about">
+            <div className="bg-black flex opacity-100 xl:opacity-0 bg-opacity-15 absolute top-0 bottom-1 w-full hover:opacity-100 transition-all duration-500 justify-center items-end rounded-lg">
+              <p className="w-full text-center text-3xl mx-0.5 text-white bg-black py-1 rounded-sm bg-opacity-65 gallery">PAINTINGS</p>
+            </div>
+            </Link>
+          </motion.div>
+          <motion.div
+          initial = {{ opacity: 0}}
+          animate = {{opacity: 1}}
+          transition={{duration: 3}} 
+          className="relative sm:w-[160%]">
+            <img src={gal2} className="rounded-md border-2 shadow-md shadow-green-300"/>
+            <Link to="/about">
+            <div className="bg-black flex opacity-100 xl:opacity-0 bg-opacity-15 absolute top-0 bottom-1 w-full hover:opacity-100 transition-all duration-500 justify-center items-end rounded-lg">
+              <p className="w-full text-center text-3xl mx-0.5 text-white bg-black py-1 rounded-sm bg-opacity-65 gallery">CRAFTS</p>
+            </div>
+            </Link>
+          </motion.div>
+          <motion.div
+          initial = {{ opacity: 0}}
+          animate = {{opacity: 1}}
+          transition={{duration: 4}}
+          className="relative sm:w-[160%]">
+            <img src={gal3} className="rounded-md border-2 shadow-md shadow-green-300"/>
+            <Link to="/about">
+            <div className="bg-black shadow-md shadow-green-300 flex opacity-100 xl:opacity-0 bg-opacity-15 absolute top-0 bottom-1 w-full hover:opacity-100 transition-all duration-500 justify-center items-end rounded-lg">
+              <p className="w-full text-center text-3xl mx-0.5 text-white border-b-2 bg-black py-1 rounded-sm bg-opacity-65 gallery">FLORA</p>
+            </div>
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+    <div className="z-50 absolute w-full">
+      <Footer />
+    </div>
+    </>
   )
 }
 
